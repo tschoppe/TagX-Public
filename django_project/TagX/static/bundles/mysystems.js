fa89770af08f02a67c1eef830233911509c51055
Nicholas Rose <njrose@umass.edu>
2017-11-27 14:46:44 -0500

    update gitignore

---
+++ django_project/TagX/static/bundles/mysystems.js
@@ -0,0 +1,26 @@
+webpackJsonp([0],{
+
+/***/ 29:
+/***/ function(module, exports, __webpack_require__) {
+
+var React = __webpack_require__(1);
+var ReactDOM = __webpack_require__(8);
+var createReactClass = __webpack_require__(7);
+
+var Goodbye = createReactClass({
+    displayName: 'Goodbye',
+
+    render() {
+        return React.createElement(
+            'h1',
+            null,
+            'React: This is the mysystems page.'
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(Goodbye, null), document.getElementById('mysystems'));
+
+/***/ }
+
+},[29]);
