2c9d8327f3c0a4c52677f6ee01096e2a00bb4787
Nicholas Rose <njrose@umass.edu>
2017-12-09 14:42:35 -0500

    partial loading feature

---
+++ TagX/static/js/nav.js
@@ -0,0 +1,7 @@
+$("li.mysystems").click(function() {
+    $("body").load("/static/partials/mySystemsSkeleton.html")
+});
+
+$("li.mygroups").click(function() {
+    $("body").load("/static/partials/myGroupsSkeleton.html")
+});
